6a0bdfd6b4a13f12ae8ff58564271681c1a21723
Fábio Silva <fabio.fernando.osilva@gmail.com>
2017-05-09 21:30:22 -0300

    Use getpwuid() to get username
    - getlogin() is deprecated

--- xeu.cpp
+++ xeu.cpp
@@ -5,16 +5,21 @@
 #include <vector>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pwd.h>
+#include <string.h>
 
 using namespace xeu_utils;
 using namespace std;
 
-int main() {
+int main()
+{
+    passwd* user = getpwuid(getuid());
+
     /* Getting username and hostname to show in shell indicator */
-    char* username = (char*) malloc(sizeof(char) * LOGIN_NAME_MAX);
+    char* username = (char*) malloc(sizeof(char) * strlen(user->pw_name) + 1);
     char* hostname = (char*) malloc(sizeof(char) * HOST_NAME_MAX);
 
-    getlogin_r(username, sizeof(char) * LOGIN_NAME_MAX);
+    strcpy(username, user->pw_name);
     gethostname(hostname, sizeof(char) * HOST_NAME_MAX);
     // ParsingState p = StreamParser().parse();
     // cout << p.dump();
